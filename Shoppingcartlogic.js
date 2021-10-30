2885de2b41e47f79e738f1a1338f596fa63127f6
radibi3000 <radibi@andrew.cmu.edu>
2021-10-30 19:16:17 -0400

    Hello

--- Shoppingcartlogic.js
+++ Shoppingcartlogic.js
@@ -12,9 +12,6 @@ let large = document.getElementById('large5')
 // Below functions, hardcode help with selecting items, on the purchase page. Changing the color and size and making sure, t
 // that only one color/size can be selected at once. 
 function toNewColor(goalColorID) {
-    // let red = document.getElementById('red5')
-    // let blue = document.getElementById('blue5')
-    // let green = document.getElementById('green5')
     red.style.backgroundColor="#FFFFFF";
     blue.style.backgroundColor="#FFFFFF";
     green.style.backgroundColor="#FFFFFF";
@@ -22,9 +19,6 @@ function toNewColor(goalColorID) {
 }
 
 function toNewSize(goalSizeID){
-    // let small = document.getElementById('small5')
-    // let medium = document.getElementById('medium5')
-    // let large = document.getElementById('large5')
     small.style.backgroundColor="#FFFFFF";
     medium.style.backgroundColor="#FFFFFF";
     large.style.backgroundColor="#FFFFFF";
@@ -32,54 +26,6 @@ function toNewSize(goalSizeID){
 
 }
 
-
-// function redToyellow()
-// {
-//     document.getElementById('red5').style.backgroundColor="#FFE600";
-//     document.getElementById('blue5').style.backgroundColor="#FFFFFF";
-//     document.getElementById('green5').style.backgroundColor="#FFFFFF";
-
-// }
-
-// function blueToyellow()
-// {
-//     document.getElementById('blue5').style.backgroundColor="#FFE600";
-//     document.getElementById('red5').style.backgroundColor="#FFFFFF";
-//     document.getElementById('green5').style.backgroundColor="#FFFFFF";
-
-// }
-
-// function greenToyellow()
-// {
-//     document.getElementById('green5').style.backgroundColor="#FFE600";
-//     document.getElementById('red5').style.backgroundColor="#FFFFFF";
-//     document.getElementById('blue5').style.backgroundColor="#FFFFFF";
-
-// }
-
-// function smallToyellow()
-// {
-//     document.getElementById('small5').style.backgroundColor="#FFE600";
-//     document.getElementById('medium5').style.backgroundColor="#FFFFFF";
-//     document.getElementById('large5').style.backgroundColor="#FFFFFF";
-
-// }
-
-// function mediumToyellow()
-// {
-//     document.getElementById('small5').style.backgroundColor="#FFFFFF";
-//     document.getElementById('medium5').style.backgroundColor="#FFE600";
-//     document.getElementById('large5').style.backgroundColor="#FFFFFF";
-
-// }
-
-// function largeToyellow()
-// {
-//     document.getElementById('large5').style.backgroundColor="#FFE600";
-//     document.getElementById('medium5').style.backgroundColor="#FFFFFF";
-//     document.getElementById('small5').style.backgroundColor="#FFFFFF";
-
-// }
 
 
 // This function updates the quantity value depending on whether you add or subtract. It also shows feedforward and less opacity
@@ -131,8 +77,21 @@ if(quantity>1)
 }});
 
 
+// To remove items from the cart
+
+var removeCartItem = document.getElementsByClassName('Remove');
+var removesize = document.getElementById('remove1');
+var removecolor = document.getElementById('remove2');
+
+    
+    button.addEventListener('click', function(event)
+    {
+     var buttonClicked = event.target
+     buttonClicked.document.removeCartItem.remove();
+    })
 
 
+    
 
-
+ 
 
